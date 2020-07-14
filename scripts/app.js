1ceb208978fdbe7b3cda91dfe04669a2eeb652c2
theFl00f <celeste.ellerby@gmail.com>
2020-07-13 20:02:20 -0400

    basic database added to app.js

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+const data = {
+    food: [
+        "kiwi", "apple", "watermelon", "starfruit", "tangerine", "blueberry", "strawberry", "blackberry", "leek", "spring onion", "banana", "cherry", "potato", "chocolate", "whipped cream", "hot sauce", "arugula", "noodles", "egg", "pizza", "plum", "peppermint"
+    ],
+    animals: [
+        "octopus", "bear", "chickadee", "bulldog", "yorkie", "turtle", "frog", "whale", "deer", "elephant", "penguin", "panther", "kitten", "betta fish", "snake", "eel", "worm", "ant", "beetle", "pufferfish", "kangaroo", "koala", "sloth", "moose", "lemur", "lion"
+    ]
+}
+
+console.log(data)

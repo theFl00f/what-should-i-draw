d6188440ab2df2a7a82911ab75a768d8c97ba155
theFl00f <celeste.ellerby@gmail.com>
2020-07-27 21:45:07 -0400

    functions to use arrays for getting random word and food

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,17 @@ const beginButton = document.getElementById('begin');
 const titleGroup = document.getElementById('title');
 const output = document.getElementById('out')
 
-const data = {
-    food: [
+const data = [
+    [
         "kiwi", "apple", "watermelon", "starfruit", "tangerine", "blueberry", "strawberry", "blackberry", "leek", "spring onion", "banana", "cherry", "potato", "chocolate", "whipped cream", "hot sauce", "arugula", "noodles", "egg", "pizza", "plum", "peppermint"
     ],
-    animals: [
+    [
         "octopus", "bear", "chickadee", "bulldog", "yorkie", "turtle", "frog", "whale", "deer", "elephant", "penguin", "panther", "kitten", "betta fish", "snake", "eel", "worm", "ant", "beetle", "pufferfish", "kangaroo", "koala", "sloth", "moose", "lemur", "lion"
     ]
-}
+]
+
+let randomNumbers = []
+let words = '';
 
 beginButton.addEventListener('click', function() {
     this.parentNode.removeChild(this);
@@ -25,5 +28,32 @@ const showGenerationPage = () => {
     output.appendChild(node);
     setTimeout(function() {
         document.querySelector('.greeting').classList.add('visible')
-    }, 500)
+    }, 1000)
+    getData()
+    console.log(words)
+}
+
+getData = () => {
+    getRandomNumbers(data);
+    getWords(data, randomNumbers)
+}
+
+const getRandomNumbers = (array) => {
+    let arr = []
+    for (let i = 0; i < 2; i++) {
+        arr[i] = Math.floor((Math.random() * array[i].length))
+    }
+    return randomNumbers = arr;
+}
+
+const getWord = (arr, num) => {
+    return arr[num]
+}
+
+const getWords = (arr1, arr2) => {
+    let arr = []
+    for (let i = 0; i < 2; i++) {
+        arr[i] = getWord(arr1[i], arr2[i])
+    }
+    return words = arr;
 }
